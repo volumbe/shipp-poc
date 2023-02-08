85c125981450b3ad2ce366bb47f286643029e155
Vivek Olumbe <vivek.olumbe@gmail.com>
2023-02-08 11:07:01 -0500

    Initial Commit

---
+++ shipp/shippApp.swift
@@ -0,0 +1,17 @@
+//
+//  shippApp.swift
+//  shipp
+//
+//  Created by Vivek Olumbe on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct shippApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
